d2ff3ee53329518b9138d66057fd5cea7adfb97b
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2024-09-23 02:02:46 +0100

    Initial commit

---
+++ test/Base.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Medchain} from "../src/Medchain.sol";
+
+contract BaseTest is Test {
+
+}
