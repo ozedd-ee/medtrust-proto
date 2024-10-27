e4b9856e4f14faae7f908ef51ebf8df55d3d50dd
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2024-10-27 23:55:31 +0100

    Add script

--- script/DeployScript.s.sol
+++ script/DeployScript.s.sol
@@ -2,11 +2,17 @@
 pragma solidity 0.8.24;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Medchain} from "../src/Medchain.sol";
 
 contract DeployScript is Script {
-    function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 testPK = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(testPK);
+        Medchain medchain = new Medchain();
+        console.log("Medchain deployed to:", address(medchain));
+
+        vm.stopBroadcast();
     }
 }
